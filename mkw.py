51b03e3e15a51df11b64d3e52bea5afb169eaa43
Justin Lam <jlam38383@gmail.com>
2023-11-23 11:01:17 -0500

    Made some edits to Racer class and created the update_position() function

--- mkw.py
+++ mkw.py
@@ -2,21 +2,22 @@ import random
 
 # Racer class
 class Racer:
-    def __init__(self, name, weight, position):
+    def __init__(self, name, weight):
         # The name of the racer (Mario, Peach, Funky Kong, etc)
         self.name = name
 
         # The weight of the racer, which will be Light, Medium, or Heavy
         self.weight = weight
 
-        # Position of the racer (int). We will assign each racer a random position to start in.
-        self.position = position
+        # Position of the racer (int). We will assign each racer a random position to start in, but for now, we set it to 0.
+        self.position = 0
 
         # Initially, each racer will have no items
         self.item = None
 
         # Initially, all racers will be behind the start line, so their distance from the start line will be negative.
-        self.distance_from_start = -1 * position
+        # But for now, we set it to 0.
+        self.distance_from_start = 0
 
         # Assigns speed values based on weight
         # random.random()/2 + 1 generates a random number between 1.0 and 1.5, and the 23, 25, and 27 are all placeholder numbers
@@ -57,6 +58,16 @@ class Item:
         if name == "Mega Mushroom" or "Lightning Cloud":
             self.speed_effect = 1.10
         if name == "Bullet Bill":
-            self.speed_effect = 1.50
-        
-        
+            self.speed_effect = 1.50   
+
+def update_position(racer1, racer2):
+    racer1.position, racer2.position = racer2.position, racer1.position
+
+def update_distance(racer):
+    pass
+
+def get_item(racer):
+    pass
+
+def use_item(racer):
+    pass
